5a987f5119d794c36c1cc7bb3b7b6083bdd27332
vinod kumar polamreddy <polamreddyvinod@bitlasoft.com>
2025-05-22 13:17:27 +0530

    password

--- db/schema.rb
+++ db/schema.rb
@@ -11,24 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.2].define(version: 2025_05_20_072640) do
-  create_table "active_storage_blobs", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
-    t.bigint "byte_size", null: false
-    t.string "checksum"
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "active_storage_variant_records", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.bigint "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
   create_table "chathistories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.text "question"
@@ -37,13 +19,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_05_20_072640) do
     t.datetime "updated_at", null: false
     t.text "result"
     t.index ["user_id"], name: "index_chathistories_on_user_id"
-  end
-
-  create_table "excel_uploads", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_excel_uploads_on_user_id"
   end
 
   create_table "sessions", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -84,8 +59,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_05_20_072640) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "chathistories", "users"
-  add_foreign_key "excel_uploads", "users"
   add_foreign_key "sql_credentials", "users"
 end
